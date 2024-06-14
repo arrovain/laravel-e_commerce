7468ac35155a66d41bf39c16a8a3ba88cdbb78e7
arrovain <arrovain@gmail.com>
2024-06-14 19:08:02 +0300

    productModel

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    public function products()
+{
+    return $this->hasMany(Product::class,'category_id');
+}
 }
