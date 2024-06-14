6d91b5f7a709fd69ad10efaf53b5b32e3b6f560e
arrovain <arrovain@gmail.com>
2024-06-14 18:45:17 +0300

    route for admin and user

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,11 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/',[AppController::class,'index'])->name('app.index');
+
+Route::middleware('auth')->group(function(){
+    Route::get('/my-account',[UserController::class,'index'])->name('user.index');
+});
+
+Route::middleware(['auth','auth.admin'])->group(function(){
+    Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
+});
