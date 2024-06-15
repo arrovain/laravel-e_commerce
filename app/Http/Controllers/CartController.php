d79d191d1ded4eb6ebfa535d9985cf26a82a59f6
arrovain <arrovain@gmail.com>
2024-06-15 15:17:42 +0300

    use Cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    use Cart;
+
+public function index()
+{
+    $cartItems = Cart::instance('cart')->content();
+    return view('cart', compact('cartItems'));
+}
 }
