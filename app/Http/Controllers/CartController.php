e9845a51ca8d0e0df9570d814e94f33b7cb91fa4
arrovain <arrovain@gmail.com>
2024-06-15 15:20:42 +0300

    remove item

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -18,4 +18,10 @@ public function updateCart(Request $request)
     Cart::instance('cart')->update($request->rowId,$request->quantity);
     return redirect()->route('cart.index');
 } 
+public function removeItem(Request $request)
+{
+    $rowId = $request->rowId;
+    Cart::instance('cart')->remove($rowId);
+    return redirect()->route('cart.index');
+}
 }
